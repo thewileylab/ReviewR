b3678e3221bab8c2babe0b749a426a70cf666bad
Luke Rasmussen <luke.rasmussen@gmail.com>
2018-08-17 07:37:10 -0500

    WIP dynamic UI for data model
    Checkpoint for refactoring dynamic creation of UI based on the
    data model

---
+++ lib/ui_helpers.R
@@ -0,0 +1,9 @@
+append_data_panel <- function(tab_id, data_table_id) {
+  print("Appending")
+  appendTab(inputId = "data_tabs", tabPanel(tab_id, withSpinner(DT::dataTableOutput(data_table_id))))
+}
+
+create_data_panel <- function(tab_id, data_table_id) {
+  print("Creating")
+  tabPanel(tab_id, withSpinner(DT::dataTableOutput(data_table_id)))
+}
