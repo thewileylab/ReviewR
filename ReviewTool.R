ceba527e3db1bf5f7fa5cb5ec6824bb935e46606
Luke Rasmussen <luke.rasmussen@gmail.com>
2018-08-17 07:37:10 -0500

    WIP dynamic UI for data model
    Checkpoint for refactoring dynamic creation of UI based on the
    data model

--- ReviewTool.R
+++ ReviewTool.R
@@ -19,13 +19,11 @@ library(shinyjs)
 library(shinydashboard)
 library(shinycssloaders)
 library(bigrquery)
-source('data_helpers.R')
-source('project_helpers.R')
-#library(shinythemes)
+source('lib/helpers.R')
 
 # Define UI for application 
 ui <- dashboardPage(
-  dashboardHeader(title = "ReviewR"),
+  dashboardHeader(title = textOutput('title')),
   dashboardSidebar(
     sidebarMenu(
       id = "tabs",
@@ -41,7 +39,7 @@ ui <- dashboardPage(
     
     tabItems(
       tabItem(tabName = "home",
-              h2("Welcome to the MIMIC Chart Reviewer"),
+              h2("Welcome to ReviewR"),
               fluidRow(class="home_container",
                 column(6,
                        div(class="jumbotron home_panel",
@@ -51,7 +49,7 @@ ui <- dashboardPage(
                 column(6,
                        div(class="jumbotron home_panel",
                          h3("Browse Patients"),
-                         div("Navigate through the full list of MIMIC III patients", class="lead"),
+                         div("Navigate through the full list of OMOP patients", class="lead"),
                          actionLink(inputId = "viewPatients", label = "View Patients", class="btn btn-primary btn-lg")))
               )
       ),
@@ -79,59 +77,28 @@ ui <- dashboardPage(
                 h4("Please select a patient from the 'Patient Search' tab")
               ),
               conditionalPanel(
-                condition = "input.subject_id",
+                condition = "input.subject_id & !output.has_projects",
+                uiOutput("patient_chart_panel_no_abstraction")
+              ), #conditionalPanel
+              conditionalPanel(
+                condition = "input.subject_id & output.has_projects",
                 splitLayout(
                   cellWidths = c("70%", "30%"),
-                  div(
-                    fluidRow(
-                      column(12, h2("You are seeing the record of:"))),
-                    fluidRow(
-                      column(12, div(class='parameters', textOutput("subject_id_output")))),
-                    fluidRow(
-                      column(12, div(id='hadm_id_display', class='parameters', textOutput("hadm_id_output")),
-                             HTML("<a id='reset_hadm_id' href='#' onclick='Shiny.onInputChange(\"hadm_id\", null);'>Clear HADM ID</a>"))),
-                    br(),
-                    fluidRow(
-                      column(12,
-                             tabsetPanel(
-                               tabPanel("ADMISSIONS", withSpinner(DT::dataTableOutput('admissions_tbl'))),
-                               tabPanel("CALLOUT", withSpinner(DT::dataTableOutput('callout_tbl'))),
-                               tabPanel("CHARTEVENTS", withSpinner(DT::dataTableOutput('chartevents_tbl'))), ## MAKE SURE TO MAP TO D_ITEMTS
-                               tabPanel("CPTEVENTS", br(), em("Please note that due to licensing restrictions, neither MIMIC-III nor course instructors can provide labels for these CPT codes."), br(), withSpinner(DT::dataTableOutput('cptevents_tbl'))),
-                               #tabPanel("DATETIMEEVENTS", DT:dataTableOutput('datetimeevents_tbl')), ## MAKE SURE TO MAP TO D_ITEMS
-                               tabPanel("DIAGNOSES_ICD", br(), em("Please note that this table has been joined with 'D_ICD_DIAGNOSES' to provide the long title value for your reference."), br(), withSpinner(DT::dataTableOutput('diagnoses_icd_tbl'))),
-                               tabPanel("DRGCODES", withSpinner(DT::dataTableOutput('drgcodes_tbl'))),
-                               tabPanel("ICUSTAYS", withSpinner(DT::dataTableOutput('icustays_tbl'))),
-                               # tabPanel("INPUTEVENTS_CV", DT::dataTableOutput('inputevents_cv_tbl')),
-                               # tabPanel("INPUTEVENTS_MV", DT::dataTableOutput('inputevents_mv_tbl')),
-                               tabPanel("LABEVENTS", br(), em("Please note that this table has been joined with 'D_LABITEMS' to provide the lab label, fluid, and category for your reference."), br(), withSpinner(DT::dataTableOutput('labevents_tbl'))),
-                               tabPanel("MICROBIOLOGYEVENTS", withSpinner(DT::dataTableOutput('microbiologyevents_tbl'))),
-                               tabPanel("NOTEEVENTS", br(), em("Please note that all 'ISERROR' notes have been removed from this table."),br(), withSpinner(DT::dataTableOutput('noteevent_tbl'))),
-                               #tabPanel("OUTPUTEVENTS", DT::dataTableOutput('outputevents_tbl')),
-                               tabPanel("PATIENTS", withSpinner(DT::dataTableOutput('patients_tbl'))),
-                               tabPanel("PRESCRIPTIONS", withSpinner(DT::dataTableOutput('prescriptions_tbl'))),
-                               tabPanel("PROCEDUREEVENTS_MV", br(), em("Please note that this table has been joined with 'D_ITEMS' to provide the Label, and DBSource for your reference."), br(), withSpinner(DT::dataTableOutput('procedureevents_mv_tbl'))),
-                               tabPanel("PROCEDURES_ICD", br(), em("Please note that this table has been joined with 'D_ICD_PROCEDURES' to provide the long title value for your reference."), br(), withSpinner(DT::dataTableOutput('procedures_icd_tbl'))),
-                               tabPanel("SERVICES", br(), em("Please note that service descriptions have been provided from the data dictionary for your reference."), br(), withSpinner(DT::dataTableOutput('services_tbl'))),
-                               tabPanel("TRANSFERS", withSpinner(DT::dataTableOutput('transfers_tbl')))
-                             )
-                      )
-                    )
-                  ),
+                  uiOutput("patient_chart_panel_abstraction"),
                   # Chart review row
                   div(
-                    fluidRow(
-                      column(12, 
-                             div(
-                               id = "abstraction_form",
-                               h4("Abstraction Results"),
-                               withSpinner(DT::dataTableOutput('abstraction_tbl'))
-                             )
-                      )
-                    )
-                  )
-                )
-              )
+                     fluidRow(
+                       column(12, 
+                              div(
+                                id = "abstraction_form",
+                                h4("Abstraction Results"),
+                                withSpinner(DT::dataTableOutput('abstraction_tbl'))
+                              ) #div
+                       ) #column
+                     ) #fluidRow
+                   ) #div
+                ) #splitLayout
+              ) #conditionalPanel
       )
     )
   )
@@ -154,6 +121,7 @@ server <- function(input, output, session) {
   names(app_config) <- app_config_df[,"key"]
   rm(app_config_df)
   
+  output$title = renderText({ paste0("ReviewR (", app_config["data_model"], ")") })
   
   if (app_config["database"] == "bigquery") {
     bq_connection_details_df <- read.csv(file="./conf/bigquery_secrets.csv", stringsAsFactors = FALSE)
@@ -178,11 +146,24 @@ server <- function(input, output, session) {
     stop("Currently this application only supports Postgres and BigQuery databases")
   }
   
+  if (tolower(app_config["data_model"]) == "mimic") {
+    render_data_tables = mimic_render_data_tables
+  }
+  else if (tolower(app_config["data_model"]) == "omop") {
+    render_data_tables = omop_render_data_tables
+  }
+  else {
+    stop("Currently this application only supports MIMIC III and OMOP data models")
+  }
+  
+  has_projects = FALSE
   if ("project_list" %in% names(app_config)) {
     tmp_project_list <- as.list(unlist(strsplit(as.character(app_config["project_list"]), split="|", fixed = TRUE)))
     projects <- do.call(rbind, lapply(tmp_project_list, loadProject))
     rm(tmp_project_list)
+    has_projects <- TRUE
   }
+  output$has_projects <- reactive({ has_projects })
   
   # Load the table mapping configuration.  For a given database system, this will tell us
   # the correct table names to use in our SQL statements
@@ -191,23 +172,7 @@ server <- function(input, output, session) {
   names(table_config) <- table_config_df[,"table_key"]
   rm(table_config_df)
 
-  admissions_data <- reactive({query_admissions(table_config, db_config, input, app_config["database"], connection)})
-  callout_data <- reactive({query_callout(table_config, db_config, input, app_config["database"], connection)})
-  chartevents_data <- reactive({query_chartevents(table_config, db_config, input, app_config["database"], connection)})
-  cptevents_data <- reactive({query_cptevents(table_config, db_config, input, app_config["database"], connection)}) 
-  diagnoses_icd_data <- reactive({query_diagnoses_icd(table_config, db_config, input, app_config["database"], connection)})
-  drgcodes_data <- reactive({query_drgcodes(table_config, db_config, input, app_config["database"], connection)})
-  icustays_data <- reactive({query_icustays(table_config, db_config, input, app_config["database"], connection)})
-  labevents_data <- reactive({query_labevents(table_config, db_config, input, app_config["database"], connection)})
-  microbiologyevents_data <- reactive({query_microbiologyevents(table_config, db_config, input, app_config["database"], connection)})
-  noteevent_data <- reactive({query_noteevent(table_config, db_config, input, app_config["database"], connection)})
-  patients_data <- reactive({query_patients(table_config, db_config, input, app_config["database"], connection)})
-  prescriptions_data <- reactive({query_prescriptions(table_config, db_config, input, app_config["database"], connection)})
-  procedureevents_mv_data <- reactive({query_procedureevents_mv(table_config, db_config, input, app_config["database"], connection)})
-  procedures_icd_data <- reactive({query_procedures_icd(table_config, db_config, input, app_config["database"], connection)})
-  services_data <- reactive({query_services(table_config, db_config, input, app_config["database"], connection)})
-  transfers_data <- reactive({query_transfers(table_config, db_config, input, app_config["database"], connection)})
-
+  output = render_data_tables(table_config, db_config, input, output, app_config["database"], connection)
   cohort_data <- reactive({ active_cohort_data(input$project_id, projects) })
   abstraction_data <- reactive({ active_abstraction_data(input$project_id, projects) %>% filter(subject_id == input$subject_id)})
   
@@ -216,42 +181,9 @@ server <- function(input, output, session) {
     select(id, project_id, name)
   
   output$subject_id_output <- renderText({paste("Subject ID - ",input$subject_id)})
-  output$hadm_id_output <- renderText({ paste("Hospital Admission ID - ", input$hadm_id) })
   output$subject_id <- renderText({input$subject_id})
-  output$hadm_id <- renderText({input$hadm_id})
-  
-  output$admissions_tbl <- DT::renderDataTable(admissions_data(), options = list(paging = FALSE, searchHighlight = TRUE),
-                                               escape=FALSE, rownames=F,
-                                               callback = JS(
-                                                 'table.on("click", "tr td a.row_hadm_id", function() {
-                                                 Shiny.onInputChange("hadm_id", $(this).text());
-                                                 });'))
-  output$callout_tbl <- DT::renderDataTable(callout_data(), options = list(paging = FALSE, searchHighlight = TRUE), rownames=F)
-  output$chartevents_tbl <- DT::renderDataTable(chartevents_data(), filter = 'top', options = list(paging = TRUE, searchHighlight = TRUE), rownames=F)
-  output$cptevents_tbl <- DT::renderDataTable(cptevents_data(), options = list(paging = FALSE, searchHighlight = TRUE), rownames=F) 
-  output$diagnoses_icd_tbl <- DT::renderDataTable(diagnoses_icd_data(), filter = 'top', options = list(paging = FALSE, searchHighlight = TRUE), rownames=F)
-  output$drgcodes_tbl <- DT::renderDataTable(drgcodes_data(), filter = 'top', options = list(paging = FALSE, searchHighlight = TRUE), rownames=F)
-  output$icustays_tbl <- DT::renderDataTable(icustays_data(), options = list(paging = FALSE, searchHighlight = TRUE), rownames=F) 
-  output$labevents_tbl <- DT::renderDataTable(labevents_data(), options = list(paging = FALSE, searchHighlight = TRUE), rownames=F)
-  output$microbiologyevents_tbl <- DT::renderDataTable(microbiologyevents_data(), options = list(paging = FALSE, searchHighlight = TRUE), rownames=F)
-  output$noteevent_tbl <- DT::renderDataTable(noteevent_data(), filter = 'top', options = list(paging = FALSE, searchHighlight = TRUE), rownames=F)
-  output$all_patients_tbl <- DT::renderDataTable(
-    query_all_patients(table_config, db_config, app_config["database"], connection),
-    options = list(paging = TRUE, pageLength = 20, searchHighlight = TRUE),
-    escape=FALSE, rownames=F,
-    callback = JS(
-      'table.on("click", "tr td a.row_subject_id", function() {
-         Shiny.onInputChange("subject_id", $(this).text());
-         $(".main-sidebar li a").click();
-       });'))
-  output$patients_tbl <- DT::renderDataTable(patients_data(), options = list(paging = FALSE, searchHighlight = TRUE), rownames=F)
-  output$prescriptions_tbl <- DT::renderDataTable(prescriptions_data(), filter = 'top', options = list(paging = FALSE, searchHighlight = TRUE), rownames=F)
-  output$procedureevents_mv_tbl <- DT::renderDataTable(procedureevents_mv_data(), filter = 'top', options = list(paging = FALSE, searchHighlight = TRUE), rownames=F)
-  output$procedures_icd_tbl <- DT::renderDataTable(procedures_icd_data(), options = list(paging = FALSE, searchHighlight = TRUE), rownames=F)
-  output$services_labels_tbl <- renderTable(services_labels)
-  output$services_tbl <-  DT::renderDataTable(services_data(), options = list(paging = FALSE, searchHighlight = TRUE), rownames=F)
-  output$transfers_tbl <- DT::renderDataTable(transfers_data(), options = list(paging = FALSE, searchHighlight = TRUE), rownames=F)
 
+  
   output$projects_tbl <- DT::renderDataTable(project_list[,c("project_id","name")], options = list(paging=FALSE, searchHighlight = TRUE),
                                              escape = FALSE, rownames=F,
                                              callback = JS(
@@ -265,10 +197,34 @@ server <- function(input, output, session) {
                                              Shiny.onInputChange("subject_id", $(this).text());
                                              $(".main-sidebar li a").click();
                                              });'))
-  output$abstraction_tbl <- DT::renderDataTable(abstraction_data(),
-                                               options = list(paging = FALSE, searchHighlight = FALSE, dom='t', ordering=FALSE),
-                                               rownames=F, edit=TRUE, selection='none')
+  # output$abstraction_tbl <- DT::renderDataTable(abstraction_data(),
+  #                                              options = list(paging = FALSE, searchHighlight = FALSE, dom='t', ordering=FALSE),
+  #                                              rownames=F, edit=TRUE, selection='none')
   
+  # Because we want to use uiOutput for the patient chart panel in multiple locations in the UI,
+  # we need to implement the workaround described here (https://github.com/rstudio/shiny/issues/743)
+  # where we actually render multiple outputs for each use.
+  patient_chart_panel = reactive({
+    print("Creating panel")
+    table_names <- get_review_table_names(app_config["data_model"])
+    #tabs <- unlist(lapply(table_names, function(id) { create_data_panel(id, paste0(id, "_tbl"))}), recursive=FALSE)
+    tabs <- lapply(table_names, function(id) { create_data_panel(id, paste0(id, "_tbl"))})
+    panel <- div(
+      fluidRow(column(12, h2("You are seeing the record of:"))),
+      fluidRow(column(12, div(class='parameters', textOutput("subject_id_output")))),
+      br(),
+      #fluidRow(column(12, tabsetPanel(id = "data_tabs", tabs)))
+      fluidRow(column(12, do.call(tabsetPanel, tabs)))
+    ) #div
+    
+    #table_names <- get_review_table_names(app_config["data_model"])
+    #tabs <- lapply(table_names, function(id) { append_data_panel(id, paste0(id, "_tbl")) })
+    panel
+  })
+  output$patient_chart_panel_abstraction <- renderUI({ patient_chart_panel() })
+  output$patient_chart_panel_no_abstraction <- renderUI( {patient_chart_panel() })
+  
+
   output$selected_project_title <- renderText({
     ifelse(is.null(input$project_id),
            "(No project selected)",
@@ -288,6 +244,8 @@ server <- function(input, output, session) {
   observeEvent(input$viewPatients, {
     updateTabsetPanel(session = session, inputId = "tabs", selected = "patient_search")
   })
+  
+  outputOptions(output, "has_projects", suspendWhenHidden = FALSE)
 }
 
 # Run the application 
