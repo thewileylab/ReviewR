26b4af982337cab276648764753f34a835a46e98
David Mayer <david.mayer@cuanschutz.edu>
2019-07-25 11:31:43 -0600

    Slight refactor -add dependency -source required module

--- app.R
+++ app.R
@@ -9,19 +9,24 @@
 # Making manual record review fun since 2019!
 #
 
-
 library(shiny)
 library(shinydashboard)
 library(shinycssloaders)
 library(tidyverse)
 library(magrittr)
+library(pool)
+source('modules/bq_auth_module.R')
 options(shiny.port = 8100)
+options(shiny.reactlog=TRUE)
 
 # Define a reactive UI for ReviewR
 ui <- dashboardPage(skin = 'red',
                     dashboardHeader(title = 'ReviewR v2.0'),
-                    dashboardSidebar(sidebarMenuOutput('application_menu')
-                                     ),
+                    dashboardSidebar(
+                        sidebarMenu(id = 'main_tabs',
+                        sidebarMenuOutput('application_menu')
+                        )
+                        ),
                     dashboardBody(
                         uiOutput('main_ui')
                         )
@@ -29,24 +34,26 @@ ui <- dashboardPage(skin = 'red',
 
 # Define server logic required to make ReviewR magic happen. 
 server <- function(input, output, session) {
-   
     ## Define a dynamic application menu
     output$application_menu <- renderMenu({
-        sidebarMenu(
+        sidebarMenu(id = 'main_tabs',
             menuItem(tabName = 'welcome', text = 'Welcome',icon = icon('home')),
-            menuItem(tabName = 'setup', text = 'Setup', icon = icon('cog'))
+            menuItem(tabName = 'setup', text = 'Setup', icon = icon('cog')),
+            menuItem(tabName = 'patient_search', text = 'Patient Serach', icon = icon('users'))
             )
         })
     
     ## Source Sidebar Menu Contents
     output$welcome_tab <- renderUI({source('ui/01_welcome.R', local = T)[1]})
     output$setup_tab <- renderUI({source('ui/02_setup.R', local = T)[1]})
+    output$patient_search_tab <- renderUI({source('ui/03_patient_search.R', local = T)[1]})
     
     ## Render the main UI
     output$main_ui <- renderUI({
         tabItems(
             tabItem(tabName = 'welcome', uiOutput('welcome_tab')),
-            tabItem(tabName = 'setup', uiOutput('setup_tab'))
+            tabItem(tabName = 'setup', uiOutput('setup_tab')),
+            tabItem(tabName = 'patient_search', uiOutput('patient_search_tab'))
             )
         })
 
