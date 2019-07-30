e3f6a4896e9cfc0f5f1029db162583968bc30c2c
David Mayer <david.mayer@cuanschutz.edu>
2019-07-30 09:33:22 -0600

    Readability - modify codeblocks for readability - modify variable names for storing module output

--- ui/02_setup.R
+++ ui/02_setup.R
@@ -1,42 +1,54 @@
 #
-# This file contains all elements that are needed to configure ReviewR and render the setup page
+# This file contains all elements that are needed to configure ReviewR and render the Setup Tab
 #
 
+# Source required setup modules----------
 source('modules/db_setup_module.R')
-db_type <- callModule(db_setup_logic, 'db_setup_ns')
-db_connection_vars <- callModule(db_connect_logic, 'db_connect_ns', db_type$db_selection )
-db_connection <- callModule(db_initialize, 'db_connect_ns', db_type$db_selection, db_connection_vars$bq_project)
+db_setup_vars <- callModule(db_setup_logic, 'db_setup_ns')
+db_connection_vars <- callModule(db_connect_logic, 'db_setup_ns', db_setup_vars$db_selection )
+db_connection <- callModule(db_initialize, 'db_setup_ns', db_setup_vars$db_selection, db_connection_vars$bq_project)
 
-
-tagList(fluidRow(box(
-  status = 'primary',
-  solidHeader = F,
-  width = 12,
-  h2("ReviewR Setup", style = 'text-align: center;')
-)),
-fluidRow(column(
-  width = 6,
-  box(
-    title = 'Connect to Patient Database',
-    width = '100%',
-    status = 'primary',
-    solidHeader = F,
-    db_setup_ui('db_setup_ns'),
-    db_connect_ui('db_connect_ns')
+# Define Setup Tab UI ----------
+tagList(
+  fluidRow(
+    box(
+      title = h2("ReviewR Setup", style = 'text-align: center;'),
+      width = 12,
+      status = 'primary',
+      solidHeader = F
+      )
+    ),
+fluidRow(
+  column(
+    width = 6,
+    box(
+      #Box Setup
+      title = 'Connect to Patient Database',
+      width = '100%',
+      status = 'primary',
+      solidHeader = F,
+      #Box Contents
+      db_setup_ui('db_setup_ns')
+      )
+    ),
+  column(
+    width = 6,
+    box(
+      #Box Setup
+      title = 'Connect to REDCap Instrument',
+      width = '100%',
+      status = 'danger',
+      solidHeader = F
+      #Box Contents
+      ),
+    box(
+      #Box Setup
+      title = 'Configure REDCap Instrument',
+      width = '100%',
+      status = 'danger',
+      solidHeader = F
+      #Box Contents
+      )
+    )
   )
-),
-column(
-  width = 6,
-  box(
-    title = 'Connect to REDCap Instrument',
-    width = '100%',
-    status = 'danger',
-    solidHeader = F
-  ),
-  box(
-    title = 'Configure REDCap Instrument',
-    width = '100%',
-    status = 'danger',
-    solidHeader = F
-  )
-)))
+)
