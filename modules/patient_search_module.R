464095274f16988171eb21043ca6124cb2c3a1ba
David Mayer <david.mayer@cuanschutz.edu>
2019-08-12 22:43:23 -0600

    Patient Search Changes - added comments - defined patient searh column order, start with ID - removed error UI and logic (replaced outside of module) - additional return, selected patient info based on JS from DT

--- modules/patient_search_module.R
+++ modules/patient_search_module.R
@@ -4,7 +4,6 @@ patient_search_ui <- function(id) {
   ns <- NS(id)
   tagList(
     dataTableOutput(ns('patient_search_dt')) %>% withSpinner() 
-    # uiOutput(ns('patient_search_error'))
   )
 }
 
@@ -20,64 +19,77 @@ patient_search_logic <- function(input, output, session, table_map, db_connectio
       ## OMOP Patient Search
       patients_omop <- 'person'
       id_omop <- 'person_id'
+      ## Determine user table from table map
       patients_table <- table_map()$model_match[[1]] %>% 
         filter(table == patients_omop) %>% 
         distinct(table, .keep_all = T) %>% 
         select(user_database_table) %>% 
         pluck(1)
+      ## Determine petient id field from table map
       subject_field <- table_map()$model_match[[1]] %>% 
         filter(table == patients_omop & field == id_omop) %>% 
         select(user_fields) %>% 
         pluck(1)
+      ## Connect to table
       tbl(db_connection(), patients_table) %>% 
         rename(ID = subject_field) %>% 
+        select(ID, everything()) %>% 
         collect()
     } else if(table_map()$count_filtered != 0 & table_map()$data_model == 'mimic3') {
       ## MIMIC Patient Search
       patients_mimic <- 'patients'
       id_mimic <- 'subject_id'
+      ## Determine user table from table map
       patients_table <- table_map()$model_match[[1]] %>% 
         filter(table == patients_mimic) %>% 
         distinct(table, .keep_all = T) %>% 
         select(user_database_table) %>% 
         pluck(1)
+      ## Determine petient id field from table map
       subject_field <- table_map()$model_match[[1]] %>% 
         filter(table == patients_mimic & field == id_mimic) %>% 
         select(user_fields) %>% 
         pluck(1)
+      ## Connect to table
       tbl(db_connection(), patients_table) %>%
         rename(ID = subject_field) %>% 
+        select(ID, everything()) %>% 
         collect()
     } else {
       return(NULL)
     }
   })
   
+  ## Render Patient Search Data Table
   output$patient_search_dt <- renderDataTable({
+    req(patient_search_tbl())
     patient_search_tbl() %>% 
-      datatable(options = list(searchHighlight = TRUE, 
+      #mutate(ID = paste0("<a class='row_subject_id' href='#'>", ID, "</a>")) %>% 
+      datatable(extensions = 'FixedColumns',
+                options = list(dom = 't',
+                               searchHighlight = TRUE, 
                                scrollX = TRUE, 
                                scrollY = '600px', 
                                search = list(regex = TRUE, 
                                              caseInsensitive = TRUE),
-                               pageLength = 25),
+                               pageLength = 25,
+                               fixedColumns = TRUE
+                               ),
                 rownames = F, 
                 selection = 'none',
                 escape = F,
-                filter = 'top')
+                filter = 'top',
+                class = 'cell-border strip hover'
+                #callback = JS('table.on("click", "tr td a.row_subject_id", function() {Shiny.onInputChange("subject_id", $(this).text());$(".main-sidebar li a").click();});')
+                ) %>% 
+      formatStyle('ID', color = '#0000EE', cursor = 'pointer')
     })
+  ## Extract the selected patient id from the patient data table
+  selected_patient <- reactive({ input$patient_search_dt_cell_clicked })
+  
+  return(list(
+    'patient_table' = patient_search_tbl,
+    'selected_patient' = selected_patient
+  ))
 }
 
-# patient_search_error_logic <- function(input, output, session, table_map, db_connection) {
-#   patient_search_error <- reactive({
-#     if (!is.null( db_connection() ) & table_map()$count_filtered == 0) {
-#         tagList(
-#           div('This database does not seem to be in OMOP or MIMIC III format?')
-#           )
-#     } else {
-#       return(NULL)
-#       }
-#   })
-#   
-#   output$patient_search_error <- renderUI({ patient_search_error() })
-# }
