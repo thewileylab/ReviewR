3b745886c5afeefc0f2143f482484a74a8b341f3
David Mayer <david.mayer@cuanschutz.edu>
2019-08-15 16:06:35 -0600

    Modularization - added modules for patient_nav and subject_info

--- ui/04_chart_review.R
+++ ui/04_chart_review.R
@@ -2,6 +2,10 @@
 # This file contains all elements that are needed to render the Chart Review Tab
 #
 
+source('modules/chart_review_module.R')
+test <- callModule(patient_nav_logic, 'chart_review', subject_info$patient_table, subject_info$dt_selection_info, parent = session)
+callModule(subject_info_logic, 'chart_review', test$subject_id)
+
 # Define Chart Review Tab UI
 output$chart_review_tab <- renderUI({
 tagList(
@@ -12,12 +16,13 @@ tagList(
         width = 9,
         box(
           #Box Setup
-          title = h2(paste('Subject ID -',patient_info$selected_patient()$value)),
+          title = h2('Subject Information'),
           width = '100%',
           height = '130px',
           status = 'success', 
-          solidHeader = F
+          solidHeader = F,
           #Box Contents
+          subject_info('chart_review')
         )
       ),
       column(
@@ -30,15 +35,7 @@ tagList(
           status = 'success', 
           solidHeader = F,
           #Box Contents
-          selectInput(inputId = 'subject_id',
-                      label = 'Subject ID', 
-                      choices = patient_info$patient_table() %>% 
-                        select(ID) %>% 
-                        deframe(),
-                      selected = patient_info$selected_patient()$value
-                      ),
-          actionButton(inputId = 'previous',label = '<--Previous',width = '155px'),
-          actionButton(inputId = 'next',label = 'Next-->',width = '155px')
+          patient_nav_ui('chart_review')
           )
         )
       )
