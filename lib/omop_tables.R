e12b8546427d5427313d136232defa21dc481d19
David Mayer <david.mayer@cuanschutz.edu>
2019-09-04 11:01:41 -0600

    Additional OMOP chart representations

--- lib/omop_tables.R
+++ lib/omop_tables.R
@@ -24,7 +24,7 @@ omop_table_all_patients <- function(table_map, db_connection) {
     mutate(Birth_Date = as_date(Birth_Date)) %>% 
     select('ID' = person_id, Gender, 'SourceVal' = person_source_value, everything()) %>% 
     arrange(ID) %>% 
-    rename_at(vars(-contains('ID',ignore.case = F)), to_title_case)
+    rename_at(vars(-1), to_title_case)
 }
 
 ## OMOP Condition ERA -----
@@ -47,7 +47,7 @@ omop_table_condition_era <- function(table_map, db_connection, subject_id) {
     select('ID' = user_field(table_map, 'condition_era','condition_era_id'), everything()) %>% 
     arrange(ID) %>% 
     collect() %>% 
-    rename_at(vars(-contains('ID',ignore.case = F)), to_title_case)
+    rename_at(vars(-1), to_title_case)
 }
 
 ## OMOP Condition Occurrence -----
@@ -74,11 +74,12 @@ omop_table_condition_occurrence <- function(table_map, db_connection, subject_id
     select('ID' = user_field(table_map,'condition_occurrence','condition_occurrence_id'), Condition, 'SourceVal' = user_field(table_map,'condition_occurrence','condition_source_value'), Status, everything(), Type, Provider, 'Visit' = user_field(table_map,'condition_occurrence','visit_occurrence_id')) %>% 
     arrange(ID) %>% 
     collect() %>% 
-    rename_at(vars(-contains('ID',ignore.case = F)), to_title_case)
-  }
+    rename_at(vars(-1), to_title_case)
+}
 ## OMOP Death -----
 
 omop_table_death <- function(table_map, db_connection, subject_id) {
+  browser()
   req(table_map(), db_connection(), subject_id() )
   message('Running Death')
   subject <- as.integer(subject_id() )
@@ -95,18 +96,175 @@ omop_table_death <- function(table_map, db_connection, subject_id) {
     left_join(death_type_concepts) %>% 
     select('ID' = person_id, 'SourceVal' = user_field(table_map, 'death', 'cause_source_value'), everything()) %>% 
     collect() %>% 
-    rename_at(vars(-contains('ID',ignore.case = F)), to_title_case)
+    rename_at(vars(-1), to_title_case)
 }
 
 ## Device Exposure -----
+omop_table_device_exposure <- function(table_map, db_connection, subject_id) {
+  req(table_map(), db_connection(), subject_id() )
+  message('Running Device Exposure')
+  subject <- as.integer(subject_id() )
+## Build Concepts
+  device_concepts <- get_subject_concept(table_map, db_connection, 'concept','concept_id','concept_name','device_exposure','device_exposure_id','device_concept_id','Device','person_id', subject)
+  device_type_concepts <- get_subject_concept(table_map, db_connection, 'concept','concept_id','concept_name','device_exposure','device_exposure_id','device_type_concept_id','Type','person_id', subject)
+  device_provider_concepts <- get_subject_concept(table_map, db_connection, 'provider','provider_id','provider_name','device_exposure','device_exposure_id','provider_id','Provider','person_id', subject)
+
+## Return Device Exposure Table Representation
+  user_table(table_map, db_connection, 'device_exposure') %>% 
+    filter(!!as.name(user_field(table_map, 'device_exposure','person_id')) == subject ) %>% 
+    left_join(device_concepts) %>% 
+    left_join(device_type_concepts) %>% 
+    left_join(device_provider_concepts) %>% 
+    select('ID' = user_field(table_map, 'device_exposure','device_exposure_id'), everything()) %>% 
+    arrange(ID) %>% 
+    collect() %>% 
+    rename_at(vars(-1), to_title_case)
+}
+ 
 ## Dose Era -----
+omop_table_dose_era <- function(table_map, db_connection, subject_id) {
+  req(table_map(), db_connection(), subject_id() )
+  message('Running Dose Era')
+  subject <- as.integer(subject_id() )
+
+## Build Concepts
+  dose_concepts <- get_subject_concept(table_map, db_connection, 'concept','concept_id','concept_name','dose_era','dose_era_id','drug_concept_id','Drug','person_id', subject)
+  unit_concepts <- get_subject_concept(table_map, db_connection, 'concept','concept_id','concept_name','dose_era','dose_era_id','unit_concept_id','Unit','person_id', subject)
+
+## Return Dose Era Table Representation
+  user_table(table_map, db_connection, 'dose_era') %>% 
+    filter(!!as.name(user_field(table_map, 'dose_era','person_id')) == subject ) %>% 
+    select(-matches('person_id|drug_concept_id|unit_concept_id')) %>% 
+    left_join(dose_concepts) %>% 
+    left_join(unit_concepts) %>% 
+    select('ID' = user_field(table_map, 'dose_era', 'dose_era_id'), Drug, Unit, 'DoseValue' = dose_value, everything()) %>% 
+    arrange(ID) %>% 
+    collect() %>% 
+    rename_at(vars(-1), to_title_case)
+}
+
 ## Drug Era -----
+omop_table_drug_era <- function(table_map, db_connection, subject_id) {
+  req(table_map(), db_connection(), subject_id() )
+  message('Running Drug Era')
+  subject <- as.integer(subject_id() )
+  
+## Build Concepts
+  drug_concepts <- get_subject_concept(table_map, db_connection, 'concept','concept_id','concept_name','drug_era','drug_era_id','drug_concept_id','Drug','person_id', subject)
+  
+## Return Drug Era Table Representation
+  user_table(table_map, db_connection, 'drug_era') %>% 
+    filter(!!as.name(user_field(table_map, 'drug_era','person_id')) == subject ) %>% 
+    select(-matches('person_id|drug_concept_id')) %>% 
+    left_join(drug_concepts) %>% 
+    select('ID' = user_field(table_map, 'drug_era', 'drug_era_id'), Drug, everything()) %>% 
+    arrange(ID) %>% 
+    collect() %>% 
+    rename_at(vars(-1), to_title_case)
+  
+}
+
 ## Drug Exposure -----
+omop_table_drug_exposure <- function(table_map, db_connection, subject_id) {
+  req(table_map(), db_connection(), subject_id() )
+  message('Running Drug Exposure')
+  subject <- as.integer(subject_id() )
+  
+## Build Concepts
+  drug_concepts <- get_subject_concept(table_map, db_connection, 'concept','concept_id','concept_name','drug_exposure','drug_exposure_id','drug_concept_id','Drug','person_id', subject)
+  drug_type_concepts <- get_subject_concept(table_map, db_connection, 'concept','concept_id','concept_name','drug_exposure','drug_exposure_id','drug_type_concept_id','Type','person_id', subject)
+  route_concepts <- get_subject_concept(table_map, db_connection, 'concept','concept_id','concept_name','drug_exposure','drug_exposure_id','route_concept_id','Route','person_id', subject)
+  provider_concepts <- get_subject_concept(table_map, db_connection, 'provider', 'provider_id', 'provider_name', 'drug_exposure', 'drug_exposure_id', 'provider_id', 'Provider', 'person_id', subject)
+
+## Return Drug Exposure Table Representation
+  user_table(table_map, db_connection, 'drug_exposure') %>% 
+    filter(!!as.name(user_field(table_map, 'drug_era','person_id')) == subject ) %>%
+    select(-matches('person_id|drug_concept_id|drug_type_concept_id|route_concept_id|provider_id|visit_detail_id|drug_source_concept_id|route_source_value|dose_unit_source_value')) %>% 
+    left_join(drug_concepts) %>% 
+    left_join(drug_type_concepts) %>% 
+    left_join(route_concepts) %>% 
+    left_join(provider_concepts) %>% 
+    select('ID' = user_field(table_map, 'drug_exposure', 'drug_exposure_id'), Drug, 'SourceVal' = user_field(table_map, 'drug_exposure', 'drug_source_value'),  'StartDate' = user_field(table_map, 'drug_exposure', 'drug_exposure_start_date'),
+           'StartDateTime' = user_field(table_map, 'drug_exposure','drug_exposure_start_datetime'), 'EndDate' = user_field(table_map, 'drug_exposure', 'drug_exposure_end_date'), 'EndDateTime' = user_field(table_map, 'drug_exposure', 'drug_exposure_end_datetime'),
+           'VerbatimEnd' = user_field(table_map, 'drug_exposure', 'drug_exposure_verbatim_end_date'), Type, 'Visit' = user_field(table_map, 'drug_exposure', 'visit_occurrence_id'), everything() ) %>% 
+    arrange(ID) %>% 
+    collect() %>% 
+    rename_at(vars(-1), to_title_case)
+  
+}
+
 ## Measurement -----
-## Note -----
-## Observation -----
-## Observation Period -----
-## Payer Plan Period -----
-## Procedure Occurrence -----
-## Specimen -----
-## Visit Occurrence -----
+omop_table_measurement <- function(table_map, db_connection, subject_id) {
+  req(table_map(), db_connection(), subject_id() )
+  message('Running Measurement')
+  subject <- as.integer(subject_id() )
+  
+## Build Concepts
+  measurement_concepts <- get_subject_concept(table_map, db_connection, 'concept','concept_id','concept_name','measurement','measurement_id','measurement_concept_id','Measurement','person_id', subject)
+  measurement_type_concepts <- get_subject_concept(table_map, db_connection, 'concept','concept_id','concept_name','measurement','measurement_id','measurement_type_concept_id','Type','person_id', subject)
+  operator_concepts <- get_subject_concept(table_map, db_connection, 'concept','concept_id','concept_name','measurement','measurement_id','operator_concept_id','Operator','person_id', subject)
+  value_concepts <- get_subject_concept(table_map, db_connection, 'concept','concept_id','concept_name','measurement','measurement_id','value_as_concept_id','Value','person_id', subject)
+  unit_concepts <- get_subject_concept(table_map, db_connection, 'concept','concept_id','concept_name','measurement','measurement_id','unit_concept_id','Unit','person_id', subject)
+  provider_concepts <- get_subject_concept(table_map, db_connection, 'provider', 'provider_id', 'provider_name', 'measurement', 'measurement_id', 'provider_id', 'Provider', 'person_id', subject)
+  
+## Return Drug Exposure Table Representation
+  user_table(table_map, db_connection, 'measurement') %>% 
+    filter(!!as.name(user_field(table_map, 'drug_era','person_id')) == subject ) %>% 
+    select(-matches('person_id|measurement_concept_id|measurement_type_concept_id|operator_concept_id|value_as_concept_id|unit_concept_id|provider_id')) %>% 
+    left_join(measurement_concepts) %>% 
+    left_join(measurement_type_concepts) %>% 
+    left_join(operator_concepts) %>% 
+    left_join(value_concepts) %>% 
+    left_join(unit_concepts) %>% 
+    left_join(provider_concepts) %>% 
+    select('ID' = user_field(table_map, 'measurement', 'measurement_id'), everything() ) %>% 
+    arrange(ID) %>% 
+    collect() %>% 
+    rename_at(vars(-1), to_title_case)
+}
+
+# ## Note -----
+# <- function(table_map, db_connection, subject_id) {
+#   req(table_map(), db_connection(), subject_id() )
+#   message('Running Condition Occurrence')
+#   subject <- as.integer(subject_id() )
+# }
+# ## Observation -----
+# <- function(table_map, db_connection, subject_id) {
+#   req(table_map(), db_connection(), subject_id() )
+#   message('Running Condition Occurrence')
+#   subject <- as.integer(subject_id() )
+# }
+# ## Observation Period -----
+# <- function(table_map, db_connection, subject_id) {
+#   req(table_map(), db_connection(), subject_id() )
+#   message('Running Condition Occurrence')
+#   subject <- as.integer(subject_id() )
+# }
+# ## Payer Plan Period -----
+# <- function(table_map, db_connection, subject_id) {
+#   req(table_map(), db_connection(), subject_id() )
+#   message('Running Condition Occurrence')
+#   subject <- as.integer(subject_id() )
+# }
+# ## Procedure Occurrence -----
+# <- function(table_map, db_connection, subject_id) {
+#   req(table_map(), db_connection(), subject_id() )
+#   message('Running Condition Occurrence')
+#   subject <- as.integer(subject_id() )
+# }
+# ## Specimen -----
+# <- function(table_map, db_connection, subject_id) {
+#   req(table_map(), db_connection(), subject_id() )
+#   message('Running Condition Occurrence')
+#   subject <- as.integer(subject_id() )
+# }
+# ## Visit Occurrence -----
+# <- function(table_map, db_connection, subject_id) {
+#   req(table_map(), db_connection(), subject_id() )
+#   message('Running Condition Occurrence')
+#   subject <- as.integer(subject_id() )
+# }
+
+
+
--- modules/patient_chart_module.R
+++ modules/patient_chart_module.R
@@ -4,7 +4,13 @@ chart_review_ui <- function(id) {
     tabsetPanel(id = 'patient_chart',type = 'tabs',
                 tabPanel(title = 'Condition Era', dataTableOutput(ns('condition_era_dt')) %>% withSpinner(type = 6)),
                 tabPanel(title = 'Condition Occurrence', dataTableOutput(ns('condition_occurrence_dt')) %>% withSpinner(type = 6)),
-                tabPanel(title = 'Death', dataTableOutput(ns('death_dt')) %>% withSpinner(type = 6))
+                tabPanel(title = 'Death', dataTableOutput(ns('death_dt')) %>% withSpinner(type = 6)),
+                tabPanel(title = 'Device Exposure', dataTableOutput(ns('device_exposure_dt')) %>% withSpinner(type = 6)),
+                tabPanel(title = 'Dose Era', dataTableOutput(ns('dose_era_dt')) %>% withSpinner(type = 6)),
+                tabPanel(title = 'Drug Era', dataTableOutput(ns('drug_era_dt')) %>% withSpinner(type = 6)),
+                tabPanel(title = 'Drug Exposure', dataTableOutput(ns('drug_exposure_dt')) %>% withSpinner(type = 6)),
+                tabPanel(title = 'Measurement', dataTableOutput(ns('measurement_dt')) %>% withSpinner(type = 6))
+                
                 )
   )
 }
@@ -29,6 +35,31 @@ chart_review_logic <- function(input, output, session, table_map, db_connection,
     omop_table_death(table_map, db_connection, subject_id)
   })
   
+  device_exposure <- reactive({
+    req(subject_id() )
+    omop_table_device_exposure(table_map, db_connection, subject_id)
+  })
+  
+  dose_era <- reactive({
+    req(subject_id() )
+    omop_table_dose_era(table_map, db_connection, subject_id)
+  })
+  
+  drug_era <- reactive({
+    req(subject_id() )
+    omop_table_drug_era(table_map, db_connection, subject_id)
+  })
+  
+  drug_exposure <- reactive({
+    req(subject_id() )
+    omop_table_drug_exposure(table_map, db_connection, subject_id)
+  })
+  
+  measurement <- reactive({
+    req(subject_id() )
+    omop_table_measurement(table_map, db_connection, subject_id)
+  })
+  
   ## Render the tibbles as datatables
   output$condition_era_dt <- renderDataTable({
     req(condition_era())
@@ -47,4 +78,36 @@ chart_review_logic <- function(input, output, session, table_map, db_connection,
     death() %>% 
       reviewr_datatable()
   })
+  
+  output$device_exposure_dt <- renderDataTable({
+    req(device_exposure() )
+    device_exposure() %>% 
+      reviewr_datatable()
+  })
+  
+  output$dose_era_dt <- renderDataTable({
+    req(dose_era() )
+    dose_era() %>% 
+      reviewr_datatable()
+  })
+  
+  output$drug_era_dt <- renderDataTable({
+    req(drug_era() )
+    drug_era() %>% 
+      reviewr_datatable()
+  })
+  
+  output$drug_exposure_dt <- renderDataTable({
+    req(drug_exposure() )
+    drug_exposure() %>% 
+      reviewr_datatable()
+  })
+  
+  output$measurement_dt <- renderDataTable({
+    req(measurement() )
+    measurement() %>% 
+      reviewr_datatable()
+  })
+  
+  
 }
