8f0dc35287f8ee85f947392846d4494ebb7991d7
David Mayer <david.mayer@cuanschutz.edu>
2019-09-05 11:51:04 -0600

    Initialize function updates -additional packages to install -additional packages to source

--- lib/initialize_reviewr.R
+++ lib/initialize_reviewr.R
@@ -4,12 +4,12 @@
 #'
 #' @examples
 initialize_reviewr <- function() {
-  message('Clear the mechanism... initializing ReviewR')
+  message('Clear the mechanism... initializing ReviewR!')
   options(shiny.port = 8100)
   options(shiny.reactlog=TRUE)
   
   # Check that Packages are installed
-  message('Checking available packages, installing those that are missing.')
+  message('Checking available packages, installing those that are missing...')
   source('lib/check_packages.R')
   check_packages(pkg = list('shiny',
                             'shinydashboard',
@@ -17,8 +17,16 @@ initialize_reviewr <- function() {
                             'shinyjqui',
                             'shinyjs',
                             'tidyverse',
+                            'tibble',
+                            'lubridate',
                             'magrittr',
-                            'DT')
+                            'bigrquery',
+                            'httr',
+                            'dbplyr',
+                            'DBI',
+                            'DT',
+                            'snakecase',
+                            'redcapAPI')
                  )
   # Load All necessary libraries
   library(shiny)
@@ -27,12 +35,21 @@ initialize_reviewr <- function() {
   library(shinyjqui)
   library(shinyjs)
   library(tidyverse)
+  library(tibble)
+  library(lubridate)
   library(magrittr)
+  library(bigrquery)
+  library(httr)
+  library(dbplyr)
+  library(DBI)
   library(DT)
+  library(snakecase)
+  library(redcapAPI)
   
   # Load Supported data models
   ## This isn't a large dataset by any means, but it can be made available across all R sessions upon App initialization. 
-  message('Loading supported data models.')
+  message('Loading supported data models...')
+  suppressMessages(
   supported_models <- list.files(path = file.path('data_models/'),full.names = T,recursive = T) %>% 
     tibble(file_path = .) %>% 
     mutate(data_model = str_extract(string = file_path, pattern = regex('(mimic3)|(omop)',ignore_case = T)),
@@ -49,6 +66,9 @@ initialize_reviewr <- function() {
            field = tolower(field)) %>% 
     group_by(file_path,data_model,model_version) %>% 
     nest(.key = cdm)
+  )
+  data_model_message <- paste('Discovered and imported', nrow(supported_models), 'supported data model versions.')
+  message(data_model_message)
   message('Proceeding to manual record review!')
   return(supported_models)
 }
