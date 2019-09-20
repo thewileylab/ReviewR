7e48589beaaa444b49e537bc2511316126a9cb3a
David Mayer <david.mayer@cuanschutz.edu>
2019-09-20 13:25:15 -0600

    variable names updated

--- lib/check_packages.R
+++ lib/check_packages.R
@@ -2,9 +2,9 @@
 # Check to see if packages are installed. Install them if they are not, then load them into the R session.
 # Courtesy of smithdanielle - https://gist.github.com/smithdanielle/9913897
 check_packages <- function(pkg){
-  new.pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
-  if (length(new.pkg)) {
-    install.packages(new.pkg, dependencies = TRUE)
+  new_pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+  if (length(new_pkg)) {
+    install.packages(new_pkg, dependencies = TRUE)
   }
   sapply(pkg, library, character.only = TRUE)
 }
