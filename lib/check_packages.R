efe5acd81dd413e03f3516579c3acdb17f9436d6
David Mayer <david.mayer@cuanschutz.edu>
2019-10-09 07:14:34 -0600

    install.packages() needs a vector, not a list

--- lib/check_packages.R
+++ lib/check_packages.R
@@ -4,7 +4,7 @@
 check_packages <- function(pkg){
   new_pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
   if (length(new_pkg)) {
-    install.packages(new_pkg, dependencies = TRUE)
+    install.packages(unlist(new_pkg), dependencies = TRUE)
   }
   sapply(pkg, library, character.only = TRUE)
 }
