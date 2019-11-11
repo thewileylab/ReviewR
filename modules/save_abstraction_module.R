ccc4daedf45c9467801a89967b4a53c30c8a902d
David Mayer <david.mayer@cuanschutz.edu>
2019-11-10 21:54:23 -0700

    Better "Instrument Complete" Implementation - initializes correctly, first time - instrument complete select input always exists, and is just shown/hidden based on other UI conditions. This prevents refresh anytime conditions change, when rendered via reactive.

--- modules/save_abstraction_module.R
+++ modules/save_abstraction_module.R
@@ -9,11 +9,6 @@ instrument_complete_ui <- function(id) {
 instrument_complete_logic <- function(input, output, session, rc_instrument, instrumentData, previousData, all_instruments, instrument_selection, subjectID) {
   ns <- session$ns
   
-  ## Debug Observer
-  observeEvent(save_abstraction_btn_press(), {
-    browser()
-  })
-  
   # Determine how many fields are required in the redcap instrument
   qty_required <- reactive({
     req(rc_instrument() )
@@ -35,6 +30,7 @@ instrument_complete_logic <- function(input, output, session, rc_instrument, ins
       nrow()
   })
   
+  # Determine the variable name of the currently selected instrument
   selected_instrument_name <- reactive({
     req(all_instruments(), instrument_selection() )
     all_instruments() %>%
@@ -42,37 +38,51 @@ instrument_complete_logic <- function(input, output, session, rc_instrument, ins
       extract2(1,1)
   })
   
-  observe({
-    instrument_complete_field <- paste0(selected_instrument_name(),'_complete')
-    updateSelectInput(session = session,
-                      inputId = 'abstraction_complete',
-                      selected = previousData() %>%
-                        select(instrument_complete_field ) %>%
-                        extract2(1) %>%
-                        as.numeric()
-                      )
+  # Create a variable containing the instrument complete field name, following the REDCap convention of instrument_name_complete
+  instrument_complete_field <- reactive({
+    req(selected_instrument_name() )
+    paste0(selected_instrument_name(),'_complete')
+  })
+  
+  # Create a reactive storing any previously stored value
+  instrument_complete_val <-reactive({
+    req(previousData(), instrument_complete_field() )
+    message('instrument_complete_val triggered')
+    previousData() %>%
+      select(instrument_complete_field() ) %>%
+      extract2(1) %>%
+      as.numeric()
     })
   
-  # If the number of required responses matches the number of required questions, show the instrument complete dropdown
-  redcap_instrument_complete <- reactive({
-    req(qty_required() == qty_required_answered() )
-    dropdown_choices <- c(0,1,2)
-    names(dropdown_choices) <- c('Incomplete', 'Unverified', 'Complete')
-    selectInput(inputId = ns('abstraction_complete'), label = 'Complete?', choices = dropdown_choices, selected = input$abstraction_complete )
+  # Create "Instrument Complete" selectInput choices as a named list
+  dropdown_choices <- c(0,1,2)
+  names(dropdown_choices) <- c('Incomplete', 'Unverified', 'Complete')
+  
+  # Initialize a select input that is hidden containing the "Instrument Complete" Choices
+  output$abstraction_complete_ui <- renderUI({
+    shinyjs::hidden(
+      div(id = ns('abstraction_complete_div'),
+          selectInput(inputId = ns('abstraction_complete'), label = 'Complete?', choices = dropdown_choices, selected = instrument_complete_val() )
+          )
+      )
     })
+  
+  # Hide or show the instrument complete field based on whether or not all required choices have been answered.  
+  observeEvent(instrumentData(), {
+    if(qty_required() == qty_required_answered() ) {
+      shinyjs::show(id = 'abstraction_complete_div')
+    } else {shinyjs::hide(id = 'abstraction_complete_div')}
+  })
+  
+  # Store Value as a reactive, to pass to other modules
   abstraction_complete_val = reactive({ input$abstraction_complete })
-  output$abstraction_complete_ui <- renderUI({ redcap_instrument_complete() })
-  
-  
-  
+  abstraction_complete <- reactive({ qty_required() == qty_required_answered() })
+    
   ## Create a Button allowing the status of the review to be saved. 
   save_abstraction_btn <- reactive({ actionButton(inputId = ns('save_abstraction'),label = "Save and Upload to REDCap") })
   save_abstraction_btn_press <- reactive({ input$save_abstraction })
   output$save_abstraction_ui <- renderUI({ save_abstraction_btn() })
   
-  abstraction_complete <- reactive({
-    qty_required() == qty_required_answered()
-  })
   return(
     list(
       'abstraction_save_btn_press' = save_abstraction_btn_press,
