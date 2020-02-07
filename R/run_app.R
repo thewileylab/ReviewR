d01b3316dc876ec43c2a9734c2fa116dd8d7ae73
David Mayer <david.mayer@cuanschutz.edu>
2020-02-07 14:57:12 -0700

    Set Default Shiny Port - when running interactively and authenticating with BigQuery, Google responds on port 8100 with OAuth credentials. By default, Shiny apps start on a random port.

--- R/run_app.R
+++ R/run_app.R
@@ -4,6 +4,7 @@
 #' @importFrom shiny shinyApp
 #' @importFrom golem with_golem_options
 run_app <- function(...) {
+  option(shiny.port = 8100)
   with_golem_options(
     app = shinyApp(ui = app_ui, server = app_server), 
     golem_opts = list(...)
