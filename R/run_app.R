deebeae8e3fa2afee18e01820bb8ebbe8542fb2f
David Mayer <david.mayer@cuanschutz.edu>
2020-02-10 11:21:36 -0700

    options fix - option -> options - allows ReviewR to always run on port 8100 for Google Auth

--- R/run_app.R
+++ R/run_app.R
@@ -3,8 +3,22 @@
 #' @export
 #' @importFrom shiny shinyApp
 #' @importFrom golem with_golem_options
+#  __________            .__              __________ 
+#  \______   \ _______  _|__| ______  _  _\______   \
+#   |       _// __ \  \/ /  |/ __ \ \/ \/ /|       _/
+#   |    |   \  ___/\   /|  \  ___/\     / |    |   \
+#   |____|_  /\___  >\_/ |__|\___  >\/\_/  |____|_  /
+#          \/     \/             \/               \/ 
+#                                       by WileyLab
+#
+# Making manual record review fun since 2019!
+#
+
+#
+# authors:  Laura Wiley, Luke Rasmussen, David Mayer
+#
 run_app <- function(...) {
-  option(shiny.port = 8100)
+  options(shiny.port = 8100)
   with_golem_options(
     app = shinyApp(ui = app_ui, server = app_server), 
     golem_opts = list(...)
