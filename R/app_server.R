72bbe52f84ab2bb25a3f2686091703292bc5b9f0
David Mayer <david.mayer@cuanschutz.edu>
2020-02-10 15:36:47 -0700

    Additional Re-arranging - more comment headers - order of operations, source modules before they are required (interim -- eventually will be part of ReviewR package env)

--- R/app_server.R
+++ R/app_server.R
@@ -1,40 +1,9 @@
 #' @import shiny
 app_server <- function(input, output, session) {
+  session$allowReconnect('force')
   # Initialize the application ----
   source('lib/initialize_reviewr.R')
   init_data <- initialize_reviewr()
-  ## Render the main UI ----
-  output$main_ui <- renderUI({
-    tabItems(
-      tabItem(tabName = 'welcome', uiOutput('welcome_tab'), class = 'active'), #https://stackoverflow.com/questions/36817407/content-doesnt-show-up-in-the-dashboard-body-if-the-sidebar-menu-is-dynamically/36819190#36819190
-      tabItem(tabName = 'setup', uiOutput('setup_tab')),
-      tabItem(tabName = 'patient_search', uiOutput('patient_search_tab')),
-      tabItem(tabName = 'chart_review', uiOutput('chart_review_tab'))
-    )
-  })
-  ## Define Main UI Outputs ----
-  ### Define a dynamic application menu
-  output$application_menu <- renderMenu({
-    sidebarMenu(id = 'main_tabs',
-                menuItem(tabName = 'welcome', text = 'Welcome',icon = icon('home')),
-                menuItem(tabName = 'setup', text = 'Setup', icon = icon('cog')),
-                menuItem(tabName = 'patient_search', text = 'Patient Search', icon = icon('users')),
-                menuItem(tabName = 'chart_review', text ='Chart Review', icon = icon('table'))
-    )
-  })
-  ### Render Application Menu Outputs
-  output$welcome_tab <- welcome_tab() 
-  output$setup_tab <- setup_tab()
-  source('ui/03_patient_search.R', local = T)$value
-  source('ui/04_chart_review.R', local = T)$value
-  
-  
-  ## Define Main UI observers ---- 
-  ## Close Application when "Leave ReviewR" button is clicked
-  observeEvent(input$quit, {
-    stopApp()
-  })
-  
   ## Source ReviewR Setup Tab Modules ----
   ## Patient Database Setup
   source('modules/db_setup_module.R')
@@ -56,8 +25,20 @@ app_server <- function(input, output, session) {
   rc_connected_vars <- callModule(redcap_connected_logic, 'abstraction_ns', abstraction_vars$rc_press, rc_project_vars$rc_project_info)
   rc_config_vars <- callModule(redcap_instrument_config_reviewer_logic, 'abstraction_ns', rc_project_vars$rc_instrument, rc_project_vars$rc_identifier, abstraction_vars$rc_con)
   rc_reconfig <- callModule(rc_instrument_configured_logic, 'abstraction_ns', rc_config_vars, instrument_selection$rc_instrument_selection)
-
-  ## Define Setup Tab UI observers which animate the ReviewR Setup Elements based on user interaction ----
+  
+  
+  ## Source Patient Search Tab Modules ----
+  ### Patient Search Module
+  source('modules/patient_search_module.R', keep.source = F)
+  subject_info <- callModule(patient_search_logic, 'patient_search_ns', table_map$table_map, db_connection_vars$db_connection, table_map$db_disconnect, subject_selection_vars$previous_sub, subject_selection_vars$next_sub, subject_selection_vars$subject_id, parent=session)
+  
+  
+  # Main UI
+  ## Define Main UI observers ---- 
+  ## Close Application when "Leave ReviewR" button is clicked
+  observeEvent(input$quit, {
+    stopApp()
+  })
   ### BigQuery Redirect Observer. When leaving the application after authenticating with BigQuery, take the user back to the Setup Tab to complete setup.
   observeEvent(db_connection_vars$bq_token(), {
     if (is.null(db_connection_vars$bq_token() ) ) { # Only redirect when the authorization token is present
@@ -66,7 +47,35 @@ app_server <- function(input, output, session) {
       updateTabItems(session, 'main_tabs', selected = 'setup')
     }
   })
+  ## Define Main UI Outputs ----
+  ### Define a dynamic application menu
+  output$application_menu <- renderMenu({
+    sidebarMenu(id = 'main_tabs',
+                menuItem(tabName = 'welcome', text = 'Welcome',icon = icon('home')),
+                menuItem(tabName = 'setup', text = 'Setup', icon = icon('cog')),
+                menuItem(tabName = 'patient_search', text = 'Patient Search', icon = icon('users')),
+                menuItem(tabName = 'chart_review', text ='Chart Review', icon = icon('table'))
+    )
+  })
+  ### Render Application Menu Outputs
+  output$welcome_tab <- welcome_tab() 
+  output$setup_tab <- setup_tab()
+  output$patient_search_tab <- patient_search_tab()
+  source('ui/04_chart_review.R', local = T)$value
   
+  ## Render the main UI ----
+  output$main_ui <- renderUI({
+    tabItems(
+      tabItem(tabName = 'welcome', uiOutput('welcome_tab'), class = 'active'), #https://stackoverflow.com/questions/36817407/content-doesnt-show-up-in-the-dashboard-body-if-the-sidebar-menu-is-dynamically/36819190#36819190
+      tabItem(tabName = 'setup', uiOutput('setup_tab')),
+      tabItem(tabName = 'patient_search', uiOutput('patient_search_tab')),
+      tabItem(tabName = 'chart_review', uiOutput('chart_review_tab'))
+    )
+  })
+  
+  
+  # Setup UI
+  ## Define Setup Tab UI observers which animate the ReviewR Setup Elements based on user interaction ----
   ### Hide/show the db_setup ui
   observeEvent(db_connection_vars$connect_press(), {
     shinyjs::hide('db_setup_div',anim = TRUE,animType = 'fade')
@@ -163,4 +172,30 @@ app_server <- function(input, output, session) {
     )
   })
   
+  
+  # Patient Search Tab UI
+  ## Define Patient Search Tab UI observers ----
+  
+  ## Patient Search Data Table Observer: open chart review tab when patient id is clicked
+  observeEvent(subject_info$dt_selection_info(), {
+    selection <- subject_info$dt_selection_info
+    if (is.null(selection()$value ) || selection()$col != 0) { # Only redirect if cell contains value and is in column 0 (Subject ID)
+      return(NULL)
+    } else {
+      updateTabItems(session, 'main_tabs', selected = 'chart_review')
+    }
+  })
+  ## Define Patient Search Tab UI Outputs ----
+  ### Create UI element for patient search DT
+  output$patient_search <- renderUI({ 
+    req(table_map$table_map() )
+    patient_search_ui('patient_search_ns') 
+    })
+  outputOptions(output, 'patient_search_tab', suspendWhenHidden = F)
+  
+  ### Create UI element from data detection module on setup tab
+  output$data_model <- renderText({
+    req(table_map$data_model_text() )
+    table_map$data_model_text() 
+  })
 }
