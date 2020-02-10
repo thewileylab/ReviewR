67c6f7198d2993496ab68702dc700c58b0bcd583
David Mayer <david.mayer@cuanschutz.edu>
2020-02-10 11:26:45 -0700

    Update License - change ownership

--- LICENSE.md
+++ LICENSE.md
@@ -1,6 +1,6 @@
 # MIT License
 
-Copyright (c) 2020 David Mayer
+Copyright (c) 2020 Laura Wiley
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
--- dev/01_start.R
+++ dev/01_start.R
@@ -26,7 +26,7 @@ golem::set_golem_options()
 ## 
 ## If you want to use the MIT licence, README, code of conduct, lifecycle badge, and news
 
-usethis::use_mit_license( name = "David Mayer" )  # You can set another licence here
+usethis::use_mit_license( name = "Laura Wiley" )  # You can set another licence here
 usethis::use_readme_rmd( open = FALSE )
 usethis::use_code_of_conduct()
 usethis::use_lifecycle_badge( "Experimental" )
