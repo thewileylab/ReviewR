1ec76e5155015a83f989b6f0303cf14816319341
David Mayer <david.mayer@cuanschutz.edu>
2020-02-18 20:10:34 -0700

    code formatting

--- R/app_ui.R
+++ R/app_ui.R
@@ -12,39 +12,33 @@ app_ui <- function() {
                   skin = 'red',
                   dashboardHeader(title = tags$a(href='https://github.com/orgs/thewileylab', target='_blank',
                                                  tags$img(src='www/logo.png', width = '125px', height = '50px')
-                  ),
-                  tags$li(class = 'dropdown', 
-                          actionButton(inputId = 'quit',label = 'Leave ReviewR',
-                                       icon = icon('suitcase-rolling')
-                          )
-                  )
-                  )
-                  ,
-                  dashboardSidebar(
-                    sidebarMenu(id = 'main_tabs',
-                                sidebarMenuOutput('application_menu')
-                    )
-                  ),
-                  dashboardBody(
-                    tags$script(stop_nav_jscode), #Prevent navigation using browser buttons (back, reload, close tab)
-                    shinyjs::useShinyjs(),
-                    uiOutput('main_ui')
+                                                 ),
+                                  tags$li(class = 'dropdown', 
+                                          actionButton(inputId = 'quit',label = 'Leave ReviewR',
+                                                       icon = icon('suitcase-rolling')
+                                                       )
+                                          )
+                                  ),
+                  dashboardSidebar(sidebarMenu(id = 'main_tabs',
+                                               sidebarMenuOutput('application_menu')
+                                               )
+                                   ),
+                  dashboardBody(tags$script(stop_nav_jscode), #Prevent navigation using browser buttons (back, reload, close tab)
+                                shinyjs::useShinyjs(),
+                                uiOutput('main_ui')
+                                )
                   )
     )
-  )
-}
+  }
 
 #' @import shiny
 golem_add_external_resources <- function(){
-
   addResourcePath('www', system.file('app/www', package = 'ReviewR') )
-  
-  tags$head(
-    golem::activate_js(),
-    golem::favicon('www/favicon.png'),
-    # Add here all the external resources
-    # If you have a custom.css in the inst/app/www
-    # Or for example, you can add shinyalert::useShinyalert() here
-    tags$link(rel="stylesheet", type="text/css", href="www/custom.css")
-  )
-}
+  tags$head(golem::activate_js(),
+            golem::favicon('www/favicon.png'),
+            # Add here all the external resources
+            # If you have a custom.css in the inst/app/www
+            # Or for example, you can add shinyalert::useShinyalert() here
+            tags$link(rel="stylesheet", type="text/css", href="www/custom.css")
+            )
+  }
