64dab603e96fa3ef153ba7b2b524b3563ebddbd4
David Mayer <david.mayer@cuanschutz.edu>
2020-08-28 12:02:56 -0600

    app_server - re-arrange: start with main UI, then move to modules - remove old setup - comment new setup

--- R/app_server.R
+++ R/app_server.R
@@ -1,19 +1,51 @@
 #' @import shiny
 app_server <- function(input, output, session) {
-  # Mod Select ----
-  ## Database Setup Vars
+  # Define Main UI Outputs ----
+  ## Define a dynamic application menu
+  output$application_menu <- renderMenu({
+    sidebarMenu(id = 'main_tabs',
+                menuItem(tabName = 'welcome', text = 'Welcome',icon = icon('home')),
+                menuItem(tabName = 'setup', text = 'Setup', icon = icon('cog')),
+                menuItem(tabName = 'patient_search', text = 'Patient Search', icon = icon('users')),
+                menuItem(tabName = 'chart_review', text ='Chart Review', icon = icon('table'))
+    )
+  })
+  
+  ## Render Application Menu Outputs
+  output$welcome_tab <- welcome_tab() 
+  output$setup_tab <- setup_tab()
+  # output$patient_search_tab <- patient_search_tab()
+  # output$chart_review_tab <- chart_review_tab()
+  
+  ## Run everything all the time
+  outputOptions(output, 'setup_tab', suspendWhenHidden = F)
+  # outputOptions(output, 'patient_search_tab', suspendWhenHidden = F)
+  # outputOptions(output, 'chart_review_tab', suspendWhenHidden = F)
+  
+  # Render Main UI ----
+  output$main_ui <- renderUI({
+    tabItems(
+      tabItem(tabName = 'welcome', uiOutput('welcome_tab'), class = 'active'), #https://stackoverflow.com/questions/36817407/content-doesnt-show-up-in-the-dashboard-body-if-the-sidebar-menu-is-dynamically/36819190#36819190
+      tabItem(tabName = 'setup', uiOutput('setup_tab'))
+      # tabItem(tabName = 'patient_search', uiOutput('patient_search_tab')),
+      # tabItem(tabName = 'chart_review', uiOutput('chart_review_tab'))
+    )
+  })
+  
+  # Setup Modules ----
+  ## Database Module Setup
   bq_setup_vars <- shinyBigQuery::bigquery_setup_server(id = 'bq-setup-namespace')
   pg_setup_vars <- shinyPostgreSQL::postgresql_setup_server(id = 'pg-setup-namespace')
-  ## DB Config
+  ## DB Var Config
   database_setup <- reactiveValues(bigquery = bq_setup_vars,
                                    postgresql = pg_setup_vars
                                    )
-  ## Abstraction Setup Vars
+  ## Abstraction Module Setup
   rc_setup_vars <- shinyREDCap::redcap_setup_server(id = 'rc-setup', reset = rc_instrument_vars$reset)
   ## Abstraction Instrument
   subject_id <- reactive({ input$subject_id }) ## Pass to instrument function
   rc_instrument_vars <- shinyREDCap::redcap_instrument_server(id = 'rc-setup', redcap_vars = rc_setup_vars, subject_id = subject_id)
-  ## Abstraction Config
+  ## Abstraction Var Config
   abs_setup <- reactiveValues(redcap = rc_setup_vars)
   
   # Setup Tab Selector Modules ----
@@ -38,13 +70,15 @@ app_server <- function(input, output, session) {
   # upload <- callModule(instrument_complete_logic, 'chart_review_upload', rc_project_vars$rc_instrument, instrumentData$instrument_data, instrumentData$previous_data, instrument_selection$rc_instruments, instrument_selection$rc_instrument_selection, subject_info$selected_patient)
   # callModule(upload_redcap_logic, 'chart_review_abstraction', abstraction_vars$rc_con, rc_project_vars$rc_record_id, rc_project_vars$rc_instrument, instrumentData$instrument_data, instrumentData$previous_data, instrumentData$current_subject, upload$abstraction_save_btn_press, upload$abstraction_complete, upload$abstraction_complete_val, instrument_selection$rc_instruments, instrument_selection$rc_instrument_selection)
   
-  ## Define Main UI observers ---- 
-  # ## Close Application when "Leave ReviewR" button is clicked
+  ## Main UI observers ---- 
+  ## Quit ReviewR Observer 
+  ### Close Application when "Leave ReviewR" button is clicked
   observeEvent(input$quit, {
     browser()
     # stopApp()
   })
-  ### BigQuery Redirect Observer. When leaving the application after authenticating with BigQuery, take the user back to the Setup Tab to complete setup.
+  ## BigQuery Redirect Observer. 
+  ### When leaving the application after authenticating with BigQuery, take the user back to the Setup Tab to complete setup.
   observeEvent(database_vars()$user_info, {
     if (is.null(database_vars()$user_info ) ) { # Only redirect when user information is present
       return(NULL)
@@ -52,64 +86,4 @@ app_server <- function(input, output, session) {
       updateTabItems(session, 'main_tabs', selected = 'setup')
     }
   })
-  
-  ## Define Main UI Outputs ----
-  ### Main UI
-  ### Define a dynamic application menu
-  output$application_menu <- renderMenu({
-    sidebarMenu(id = 'main_tabs',
-                menuItem(tabName = 'welcome', text = 'Welcome',icon = icon('home')),
-                menuItem(tabName = 'setup', text = 'Setup', icon = icon('cog')),
-                menuItem(tabName = 'patient_search', text = 'Patient Search', icon = icon('users')),
-                menuItem(tabName = 'chart_review', text ='Chart Review', icon = icon('table'))
-    )
-  })
-  ### Render Application Menu Outputs
-  output$welcome_tab <- welcome_tab() 
-  output$setup_tab <- setup_tab()
-  # output$patient_search_tab <- patient_search_tab()
-  # output$chart_review_tab <- chart_review_tab()
-  ### Run everything all the time
-  outputOptions(output, 'setup_tab', suspendWhenHidden = F)
-  # outputOptions(output, 'patient_search_tab', suspendWhenHidden = F)
-  # outputOptions(output, 'chart_review_tab', suspendWhenHidden = F)
-
-  ## Render the main UI ----
-  output$main_ui <- renderUI({
-    tabItems(
-      tabItem(tabName = 'welcome', uiOutput('welcome_tab'), class = 'active'), #https://stackoverflow.com/questions/36817407/content-doesnt-show-up-in-the-dashboard-body-if-the-sidebar-menu-is-dynamically/36819190#36819190
-      tabItem(tabName = 'setup', uiOutput('setup_tab'))
-      # tabItem(tabName = 'patient_search', uiOutput('patient_search_tab')),
-      # tabItem(tabName = 'chart_review', uiOutput('chart_review_tab'))
-      )
-  })
-  
-  # # Setup UI
-  # 
-  # output$model <- renderUI({ 
-  #   div(id = 'data_model_div',
-  #       data_model_detection_ui('model_ns')
-  #   )
-  # })
-  # 
-  # # Patient Search Tab UI
-  # ## Define Patient Search Tab UI observers ----
-  # 
-  # ## Patient Search Data Table Observer: open chart review tab when patient id is clicked
-  # observeEvent(subject_info$dt_selection_info(), {
-  #   selection <- subject_info$dt_selection_info
-  #   if (is.null(selection()$value ) || selection()$col != 0) { # Only redirect if cell contains value and is in column 0 (Subject ID)
-  #     return(NULL)
-  #   } else {
-  #     updateTabItems(session, 'main_tabs', selected = 'chart_review')
-  #   }
-  # })
-  # ## Define Patient Search Tab UI Outputs ----
-  # outputOptions(output, 'patient_search_tab', suspendWhenHidden = F)
-  # 
-  # ### Create UI element from data detection module on setup tab
-  # output$data_model <- renderText({
-  #   req(table_map$data_model_text() )
-  #   table_map$data_model_text() 
-  # })
 }
