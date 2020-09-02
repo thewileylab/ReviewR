530268ed6e0c792b4eb3ab37fd39dd72b0c7e20e
David Mayer <david.mayer@cuanschutz.edu>
2020-09-02 10:32:52 -0600

    move data_supported_datamodels to mod_datamodel_setup

--- R/mod_datamodel_setup.R
+++ R/mod_datamodel_setup.R
@@ -53,6 +53,26 @@ user_field <- function(table_map, desired_cdm_table, desired_cdm_field){
     pluck(1)
 }
 
+# Datasets ----
+#' ReviewR Supported Database Schemas
+#'
+#' A dataset containing data model information along with the corresponding
+#' version and schema.
+#' 
+#' @docType data
+#'
+#' @format A data frame with 12 rows and 4 variables:
+#' \describe{
+#'   \item{file_path}{Where schema was imported from}
+#'   \item{datamodel}{Data model name, OMOP or MIMIC}
+#'   \item{model_version}{Version of the data model}
+#'   \item{data}{Nested database schemas, including included table and field mappings}
+#'   ...
+#' }
+#' @source \url{https://github.com/OHDSI/CommonDataModel/}
+#' @source \url{https://github.com/MIT-LCP/mimic-code}
+"supported_datamodels"
+
 # UI ----
 #' Data Model Detection Module
 #'
