d9d88d1de7be90d6c23f48a9d5adeba3046ccca8
David Mayer <david.mayer@cuanschutz.edu>
2020-09-02 16:17:30 -0600

    mod_chartreview - create view/review layouts - use chartreview_ui() function to render view/review - add abstraction instrument to mod_chartreview

--- R/mod_chartreview.R
+++ R/mod_chartreview.R
@@ -11,7 +11,8 @@
 chartreview_ui <- function(id) {
   ns <- NS(id)
   tagList(
-  
+    actionButton(inputId = ns('debug'), label = "I'm alive!"),
+    uiOutput(ns('view_review'))
   )
 }
 
@@ -22,16 +23,44 @@ chartreview_ui <- function(id) {
 #' @param database_vars Database variables returned from user selected database setup module
 #' @param datamodel_vars Datamodel variables returned from mod_datamodel_setup
 #' @param abstract_vars Abstraction variables returned from user selected abstraction module
+#' @param navigation_vars Navigation variables returned from mod_navigation
 #'
 #' @rdname mod_chartreview
 #' 
 #' @keywords internal
 #' @export
-mod_chartreview_server <- function(id, database_vars, datamodel_vars, abstract_vars) {
+mod_chartreview_server <- function(id, database_vars, datamodel_vars, abstract_vars, navigation_vars) {
   moduleServer(
     id,
     function(input, output, session) {
       
+      observeEvent(input$debug, {
+        browser()
+        })
+      
+      view_review <- reactive({
+        if(database_vars()$is_connected == 'yes' & abstract_vars()$is_configured == 'yes') {
+          tagList(
+            column(width = 9,
+                   box(width = '100%',
+                       status = 'primary'
+                       ## Patient Chart
+                       )
+                   ),
+            column(width = 3,
+                   ## Abstraction Instrument
+                   abstraction_instrument_ui('abs-selector')
+                   )
+            )
+        } else {
+          tagList(
+            box(width = '100%',
+                status = 'primary')
+            )
+          }
+      })
+      
+      output$view_review <- renderUI({ view_review() })
     }
   )
 }
