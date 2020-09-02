846904561cc5a8510cf142ad6456c1d72c657930
David Mayer <david.mayer@cuanschutz.edu>
2020-09-02 15:10:55 -0600

    mod_chartreview - use module_server from Shiny 1.5 - add roxygen

--- R/mod_chartreview.R
+++ R/mod_chartreview.R
@@ -1,3 +1,13 @@
+# UI ----
+#' Chart Review
+#'
+#' @param id The namespace id for the UI output
+#'
+#' @rdname mod_chartreview
+#' 
+#' @keywords internal
+#' @export
+#'
 chartreview_ui <- function(id) {
   ns <- NS(id)
   tagList(
@@ -5,6 +15,20 @@ chartreview_ui <- function(id) {
   )
 }
 
-mod_chartreview_server <- function(input, output, session) {
-  
+# Server ----
+#' Chart Review
+#'
+#' @param id 
+#'
+#' @rdname mod_chartreview
+#' 
+#' @keywords internal
+#' @export
+mod_chartreview_server <- function(id) {
+  moduleServer(
+    id,
+    function(input, output, session) {
+      
+    }
+  )
 }
