82f3d8ef39ab318618e17a1dd2bc5df03c960714
David Mayer <david.mayer@cuanschutz.edu>
2020-09-04 10:49:43 -0600

    Update comment structure - add information - add clarity (hopefully)

--- R/app_server.R
+++ R/app_server.R
@@ -16,19 +16,15 @@ app_server <- function(input, output, session) {
                 )
     })
   
-  ## Render Application Menu Outputs
+  ## Application Menu Outputs
+  ### Assign layout functions to Dashboard Tab Outputs
   output$homepage <- homepage()
   output$setup <- setup()
   output$patient_search <- patient_search()
   output$chart_review <- chart_review()
   
-  ## Render ReviewR Version Info
-  output$reviewr_version <- renderUI({ 
-    HTML(glue::glue("<small>Version: {packageDescription(pkg = 'ReviewR', fields = 'Version')}</small>") )
-    })
-  
-  ## Run everything all the time
-  ### Certain observers won't fire correctly without this set and if they are located on a tab that isn't in focus
+  ## Initialize All Dashboard Tabs
+  ### Occasionally, observers will not trigger if they are located on a tab that has not been initialized.
   outputOptions(output, 'homepage', suspendWhenHidden = F)
   outputOptions(output, 'setup', suspendWhenHidden = F)
   outputOptions(output, 'patient_search', suspendWhenHidden = F)
@@ -37,11 +33,17 @@ app_server <- function(input, output, session) {
   ## Render Main UI
   output$main_ui <- renderUI({
     tabItems(
-      tabItem(tabName = 'welcome', uiOutput('homepage'), class = 'active'), #https://stackoverflow.com/questions/36817407/content-doesnt-show-up-in-the-dashboard-body-if-the-sidebar-menu-is-dynamically/36819190#36819190
+      ### https://stackoverflow.com/questions/36817407/content-doesnt-show-up-in-the-dashboard-body-if-the-sidebar-menu-is-dynamically/36819190#36819190
+      tabItem(tabName = 'welcome', uiOutput('homepage'), class = 'active'), 
       tabItem(tabName = 'setup', uiOutput('setup')),
       tabItem(tabName = 'patient_search', uiOutput('patient_search')),
       tabItem(tabName = 'chart_review', uiOutput('chart_review'))
       )
+    })
+  
+  ## Render ReviewR Version Info
+  output$reviewr_version <- renderUI({ 
+    HTML(glue::glue("<small>Version: {packageDescription(pkg = 'ReviewR', fields = 'Version')}</small>") )
     })
   
   # Main UI Observers ---- 
@@ -62,15 +64,14 @@ app_server <- function(input, output, session) {
   # Setup Modules ---- 
   ## Database
   database_vars <- mod_database_setup_server('db-selector')
-  
-  ## Database Detection Module
+  ## Database Detection 
   datamodel_vars <- mod_datamodel_detection_server('data-model', database_vars, navigation_vars)
-  
   ## Abstraction
   abstract_vars <- mod_abstraction_setup_server('abs-selector', selected_subject_id)
   
   # Patient Navigation Module ----
   navigation_vars <- mod_navigation_server('pt-navigation', database_vars, datamodel_vars, abstract_vars, session)
+  ## Encapsulate 'selected_subject_id' return as a reactive, to be used in abstraction modules
   selected_subject_id <- reactive({ navigation_vars$selected_subject_id })
 
   # Chart Review Module ----
