1f8e5c8081a3dd8413e0d5a1cb665e6316d3959e
David Mayer <david.mayer@cuanschutz.edu>
2020-09-24 13:12:15 -0600

    update dev_database_module - open module file, after creation - validate function arguments - add param descriptions, with examples

--- R/reviewr_dev.R
+++ R/reviewr_dev.R
@@ -13,20 +13,25 @@
 #'
 #' This function will assist in providing a database module skeleton for ReviewR
 #'
-#' @param mod_name 
-#' @param display_name 
+#' @param mod_name A string, denoting the module suffix eg: 'mariadb'
+#' @param display_name A string, denoting the module display name eg: 'MariaDB'
 #'
 #' @return
 #'
 #' @importFrom glue glue glue_collapse
 #' @importFrom purrr map
-#' @examples
-dev_database_module <- function(mod_name, display_name) {
-  filename <- glue::glue('mod_{mod_name}.R')
-  cat(glue::glue_collapse(x = map(ReviewR::db_module_template,
-                                  ~glue::glue(.x)),
-                          sep = '\n' 
-                          ),
-      file = glue::glue('R/{filename}') 
-      )
-}
+#' @importFrom rstudioapi navigateToFile
+dev_database_module <- function(mod_name = NULL, display_name = NULL) {
+  if(is.null(mod_name) | is.null(display_name)) {
+    message('mod_name and display_name are required arguments.')
+    } else {
+      filename <- glue::glue('R/mod_{mod_name}.R')
+      cat(glue::glue_collapse(x = map(ReviewR::db_module_template,
+                                      ~glue::glue(.x)),
+                              sep = '\n' 
+                              ),
+          file = filename
+          ) 
+          rstudioapi::navigateToFile( filename )
+      }
+  }
