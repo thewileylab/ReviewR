3d7106f27b1537039edd3af8185c41bf51189780
David Mayer <david.mayer@cuanschutz.edu>
2020-09-25 10:05:22 -0600

    begin dev_add_datamodel cleanup - add roxygen - use function templates from ReviewR namespace - add comments - remove old code

--- R/reviewr_dev.R
+++ R/reviewr_dev.R
@@ -8,6 +8,26 @@
 #' @format A character vector with 51 elements
 "db_module_template"
 
+#' ReviewR Database Function: All Patients Table Template
+#'
+#' A character vector containing a function template for creating the 
+#' 'All Patients' table as displayed on the "Patient Search" Tab
+#' 
+#' @docType data
+#'
+#' @format A character vector with 19 elements
+"db_function_all_patients_table_template"
+
+#' ReviewR Database Function: Subject Table Template
+#'
+#' A character vector containing a function template for creating the 
+#' 'Subject Filtered' tables as displayed on the "Chart Review" Tab
+#'  
+#' @docType data
+#'
+#' @format A character vector with 13 elements
+"db_function_subject_table_template"
+
 # Dev Functions ----
 #' Develop Database Module
 #'
@@ -34,42 +54,57 @@ dev_database_module <- function(mod_name = NULL, display_name = NULL) {
       }
 }
 
-
-####
-
-csv <- '~/Desktop/test_v1.4.csv'
-all_patients <- 'table_1'
-all_patients_table_template <- readLines('data-raw/templates/template_all_patients_table')
-subject_table_template <- readLines('data-raw/templates/template_subject_table')
-
-library(tidyverse)
-dev_add_datamodel <- function(csv, all_patients) {
+#' Develop Datamodel Functions
+#' 
+#' This function will assist in adding a new supported datamodel to 
+#' ReviewR. A CDM file, stored as a CSV will be added to the 
+#' namespace, such that the CDM can be identified by the datamodel
+#' detection module. Additionally, a database_tables.R file will 
+#' be created and opened in R/ with basic table skeletons created
+#' based on the CDM stored in the user supplied CSV.
+#'
+#' @param csv 
+#' @param all_patients_table 
+#' @param patient_identifier_field 
+#' 
+#' @importFrom dplyr distinct filter mutate pull
+#' @importFrom glue glue glue_collapse
+#' @importFrom magrittr %>% extract2
+#' @importFrom purrr map imap
+#' @importFrom stringr str_remove_all str_split
+#' @importFrom tibble enframe
+#' @importFrom tidyr replace_na separate
+#' @importFrom rlang .data
+dev_add_datamodel <- function(csv, all_patients_table, patient_identifier_field) {
+  ## Add user supplied CSV file to package and incorporate into ReviewR::supported_datamodels.rda
   file.copy(from = csv, to = 'data-raw/datamodels/')
   supported_datamodels <- list.files(path = file.path('data-raw/datamodels'),full.names = T,recursive = T) %>%
     tibble::enframe(name = NULL, value = 'file_path') %>% 
-    mutate(datamodel = basename(file_path),
-           datamodel = str_remove_all(datamodel, '.csv')
-    ) %>% 
-    separate(col = datamodel, into = c('datamodel','model_version'), sep = '_', extra = 'drop', fill = 'right') %>% 
-    mutate(model_version = tidyr::replace_na(model_version, ''),
-           cdm = map(file_path,
+    mutate(datamodel = basename(.data$file_path),
+           datamodel = str_remove_all(.data$datamodel, '.csv')
+           ) %>% 
+    separate(col = .data$datamodel, into = c('datamodel','model_version'), sep = '_', extra = 'drop', fill = 'right') %>% 
+    mutate(model_version = tidyr::replace_na(.data$model_version, ''),
+           cdm = map(.data$file_path,
                      ~read_csv(.x)
-           )
-    ) %>% 
-    unnest(cols = cdm) %>% 
-    mutate(joinable_table = tolower(table),
-           joinable_field = tolower(field)
-    ) %>% 
-    group_by(file_path,datamodel,model_version) %>% 
+                     )
+           ) %>% 
+    unnest(cols = .data$cdm) %>% 
+    mutate(joinable_table = tolower(.data$table),
+           joinable_field = tolower(.data$field)
+           ) %>% 
+    group_by(.data$file_path,.data$datamodel,.data$model_version) %>% 
     nest()
-  
   # usethis::use_data(supported_datamodels, overwrite = T)
+  
+  ## Determine Datamodel moniker and version
   temp_datamodel <-basename(csv) %>%
     str_remove_all('.csv') %>% 
     str_split(pattern = '_')
   new_datamodel <- temp_datamodel[[1]][1]
   new_datamodel_version <- temp_datamodel[[1]][2]
   
+  ## Discover Tables that should potentially be rendered
   new_tables <- supported_datamodels %>% 
     filter(datamodel == new_datamodel & model_version == new_datamodel_version ) %>% 
     pull(data) %>% 
@@ -78,15 +113,15 @@ dev_add_datamodel <- function(csv, all_patients) {
     filter(table != all_patients) %>% 
     pull(table)
   
-  ## All Patients
-  glue::glue_collapse(x = map(all_patients_table_template,
+  ## Create All Patients Table from template
+  glue::glue_collapse(x = map(ReviewR::db_function_all_patients_table_template,
                               ~glue::glue(.x)
                               ),
                       sep = '\n')
   ## Subject Tables
   test <- imap(new_tables,
               ~{new_table <- new_tables[.y]
-                glue::glue_collapse(x = map(subject_table_template,
+                glue::glue_collapse(x = map(ReviewR::db_function_subject_table_template,
                                             ~glue::glue(.x)
                                             ),
                                     sep = '\n'
@@ -94,10 +129,4 @@ dev_add_datamodel <- function(csv, all_patients) {
       )
   map(test,
       ~cat(.x, file = '~/Desktop/cat_test',append = T))
-  # test<-map_ch(subject_table_template,
-  #          ~glue::glue(.x)
-  #          )
-  # test2<-map_chr(test,
-  #     ~glue::glue_collapse(.x, sep = '\n')
-  #     )
 }
