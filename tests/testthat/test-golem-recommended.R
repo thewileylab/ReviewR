ed0ff78151166994ac2fefe7735c7c3e5d9562d6
David Mayer <david.mayer@cuanschutz.edu>
2021-01-13 14:22:07 -0700

    CRAN checks - Update DESCRIPTION: Title needs to be title case and not begin with package name or 'this package' - update wordlist for CMD check GitHub actions badge - modify test structure to work with CRAN released version of golem. Define expect_running() function manually until next version of golem is available. - add here to suggests, as it is now used directly in tests

--- tests/testthat/test-golem-recommended.R
+++ tests/testthat/test-golem-recommended.R
@@ -9,9 +9,26 @@ test_that("app server", {
 })
 
 # Configure this test to fit your need
+# Define function manually, until golem updated beyond v0.2.1 on CRAN
+expect_running <- function(sleep){
+  skip_on_cran()
+  skip_on_travis()
+  skip_on_appveyor()
+  x <- processx::process$new(
+    "R", 
+    c(
+      "-e", 
+      "pkgload::load_all(here::here());run_app()"
+    )
+  )
+  Sys.sleep(sleep)
+  expect_true(x$is_alive())
+  x$kill()
+} 
 test_that(
   "app launches",{
     skip_if_not(interactive())
-    golem::expect_running(sleep = 5)
+    # golem::expect_running(sleep = 5)
+    expect_running(sleep = 5)
   }
 )
